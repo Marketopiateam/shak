5fcce2025005e055d952c8ab24705d7ed0d9c4e9
RichnessInc <omarkhaledjob2001@gmail.com>
2024-08-04 10:48:04 +0300

    Commit

--- app/Http/Controllers/Api/V1/Admin/PaymentsApiController.php
+++ app/Http/Controllers/Api/V1/Admin/PaymentsApiController.php
@@ -19,6 +19,7 @@ class PaymentsApiController extends Controller
     }
     public function charge_wallet(Request $request)
     {
+        dd($request->value);
         $payment        = $this->tap($request->value);       
         $redirect_url   = $payment['redirect_url'];
         return    resp($redirect_url, 'success', 200);
